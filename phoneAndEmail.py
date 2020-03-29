d8c456056f3d896ddf1d308104eb18f58c91e412
Matthew Landes <mlandes.jm@gmail.com>
2020-03-29 13:45:48 -0400

    fixed errors

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -23,22 +23,22 @@ emailRegex = re.compile(r'''(
     [a-zA-Z0-9._%+-]+               # username
     @                               # @ symbol
     [a-zA-Z0-9.-]+                  # domain
-    (\.[a-zA-Z]{2,4}                # dot-something
+    (\.[a-zA-Z]{2,4})                # dot-something
     )''', re.VERBOSE)
 
 # Finds matches in clipboard text
 text = str(pyperclip.paste())
 matches = []
 for groups in phoneRegex.findall(text):
-    phoneNum = '-'.join([[groups[1], groups[3], groups[5]])
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
     if groups[8] != '':
         phoneNum += ' x' + groups[8]
     matches.append(phoneNum)
 for groups in emailRegex.findall(text):
-    matches.append(group[0])
+    matches.append(groups[0])
 
 # Copy results to the clipboard
-if len(matched) > 0:
+if len(matches) > 0:
     pyperclip.copy('\n'.join(matches))
     print('Copied to clipboard:')
     print('\n'.join(matches))
